bc94ecc43a707998cfb5fa620d0088cd137aacee
Chandrima Kar <chandrimakar16@gmail.com>
2024-09-02 00:38:44 +0530

    typedefs updated

---
+++ server/typedefs/heritage.js
@@ -0,0 +1,107 @@
+import { gql } from "apollo-server-express";
+
+const heritageTypeDefs = gql`
+  type Query {
+    getHeritage(id: ID!): Heritage
+    getHeritages: [Heritage]
+  }
+
+  type Mutation {
+    createHeritage(input: HeritageInput!): Heritage!
+    updateHeritage(id: ID!, input: HeritageInput!): Heritage!
+    deleteHeritage(id: ID!): AuthResponse!
+  }
+
+  type Heritage {
+    _id: ID!
+    name: String!
+    image: Photo
+    introduction: String!
+    endlessDigitalArt: Video
+    animatedVideo: Video
+    vlogVideo: Video
+    part1: [Description]
+    part2: [Description]
+    type_of_heritage: HeritageType!
+    tag: String
+    police_helpline: String!
+    women_helpline: String!
+    child_helpline: String!
+    fire_emergency: String!
+    medical_emergency: String!
+    state_culture_name: State
+    state_culture_link: [ID]
+    entry_fee: Float
+    nearest_attraction: [ID]
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Description {
+    heading: String!
+    description: String!
+  }
+
+  type HeritageType {
+    type: String!
+    image: Photo!
+  }
+
+  type State {
+    name: String!
+    image: Photo!
+  }
+
+  type Video {
+    ETag: String
+    ServerSideEncryption: String
+    Location: String
+    Key: String
+    key: String
+    Bucket: String
+  }
+
+  input HeritageInput {
+    name: String!
+    image: PhotoInput
+    introduction: String!
+    endlessDigitalArt: MediaInput
+    animatedVideo: MediaInput
+    vlogVideo: MediaInput
+    part1: [DescriptionInput]
+    part2: [DescriptionInput]
+    type_of_heritage: HeritageTypeInput!
+    tag: String
+    police_helpline: String!
+    women_helpline: String!
+    child_helpline: String!
+    fire_emergency: String!
+    medical_emergency: String!
+    state_culture_name: StateInput!
+    state_culture_link: [ID]
+    entry_fee: Float
+    nearest_attraction: [ID]
+  }
+
+  input DescriptionInput {
+    heading: String!
+    description: String!
+  }
+
+  input HeritageTypeInput {
+    type: String!
+    image: PhotoInput!
+  }
+
+  input StateInput {
+    name: String!
+    image: PhotoInput!
+  }
+
+  input PhotoInput {
+    url: String
+    public_id: String
+  }
+`;
+
+export default heritageTypeDefs;
