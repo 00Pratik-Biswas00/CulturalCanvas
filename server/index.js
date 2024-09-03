4ffb60fb44a067455989c925b8892703dc44a268
Rupal Paul <rupalpaulgspr@gmail.com>
2024-09-03 01:21:51 -0700

    [Add] Cloud Upload

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
 import mongoose from "mongoose";
-import cloudinary from "cloudinary";
 import cors from "cors";
 import uploadRoutes from "./routes/upload.js";
 import { createServer } from "http";
@@ -64,16 +63,6 @@ await apolloServer.start();
 
 apolloServer.applyMiddleware({ app });
 
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-
-app.use("/api", uploadRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Server is running...");
-});
-
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -84,13 +73,10 @@ mongoose
     process.exit(1);
   });
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-
-// app.use("/api", uploadRoutes);
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use("/api", uploadRoutes);
 
 http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
