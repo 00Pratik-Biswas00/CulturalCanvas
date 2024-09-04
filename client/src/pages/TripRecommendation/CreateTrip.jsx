8aeb01d150b8e99ce026111207782368611e62dc
Pratik Biswas <04biswaspratik@gmail.com>
2024-09-04 11:24:51 +0530

    trip recommendation is updated

---
+++ client/src/pages/TripRecommendation/CreateTrip.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateTrip = () => {
+  return (
+    <section className="bg-background1 dark:bg-dark_background1 text-primary_text dark:text-dark_primary_text py-4 px-16 duration-300">
+      Create Trip
+    </section>
+  );
+};
+
+export default CreateTrip;
---
+++ client/src/pages/TripRecommendation/PredictAmount.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PredictAmount = () => {
+  return (
+    <section className="bg-background1 dark:bg-dark_background1 text-primary_text dark:text-dark_primary_text py-4 px-16 duration-300">
+      Predict Amount
+    </section>
+  );
+};
+
+export default PredictAmount;
