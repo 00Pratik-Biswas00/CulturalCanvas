08ee4d63ff286be82b3b38cb68d3874000bf3687
Rupal Paul <rupalpaulgspr@gmail.com>
2024-09-06 01:37:45 -0700

    [Add] Chatbot Backend

--- client/src/pages/ExploreDiversity/ViewTrip/[tripId]/index.jsx
+++ client/src/pages/ExploreDiversity/ViewTrip/[tripId]/index.jsx
@@ -1,37 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { db } from "../../../../components/ExplorePlacesComponents/service/firebaseConfig";
-import { doc, getDoc } from "firebase/firestore";
 import { useParams } from "react-router-dom";
-import { toast } from "sonner";
-
 import InfoSection from "../viewtrip-components/InfoSection";
 import Hotels from "../viewtrip-components/Hotels";
 import TripPlace from "../viewtrip-components/TripPlace";
 import Footer from "../viewtrip-components/Footer";
+import api from "../../../../config/axios";
 
 function ViewTrip() {
   const { tripId } = useParams();
-  const [trip, setTrip] = useState();
+  const [trip, setTrip] = useState({});
   const GetTripData = async () => {
-    const docRef = doc(db, "AiTrips", tripId);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      setTrip(docSnap.data());
-    } else {
-      console.log("No such document!");
-      toast("No trip found!");
-    }
+    const { data } = await api.get(`/trips/${tripId}`);
+    setTrip(data);
   };
   useEffect(() => {
     tripId && GetTripData();
   }, [tripId]);
-  // Get Trip Info DAta from Firebase
-
-  useEffect(() => {
-    tripId && GetTripData();
-  }, [tripId]);
-
   return (
     <div className="p-12 md:px-25 lg:px-44 xl:px:56">
       <InfoSection trip={trip} />
---
+++ server/config.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import cloudinary from "cloudinary";
+import AWS from "aws-sdk";
+import "dotenv/config";
+
+const connect = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGODB_URI)
+      .then(() => {
+        console.log("Database connected successfully.");
+      })
+      .catch((error) => {
+        console.log("Error connecting to database: " + JSON.stringify(error));
+        process.exit(1);
+      });
+
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+const awsConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
+
+export const S3 = new AWS.S3(awsConfig);
+export default connect;
