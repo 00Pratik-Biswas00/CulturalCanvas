cd940bcac0d07db462cd01e4893a1a92a6eaad61
Pratik Biswas <04biswaspratik@gmail.com>
2024-10-28 10:38:26 +0530

    updated blog page

--- client/src/pages/SingleBlogVlog/index.jsx
+++ client/src/pages/SingleBlogVlog/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { CiEdit } from "react-icons/ci";
+import { CiEdit, CiLocationOn } from "react-icons/ci";
+import { useNavigate } from "react-router-dom";
+
 import MyButton4 from "../../components/Buttons/MyButton4";
 import img1 from "../../assets/blogs/pic3.png";
 import ReactPlayer from "react-player";
 import demovid from "../../assets/Heritage/bais.mp4";
 import SquareAnimation from "../../components/Blobs/SquareAnimation";
+import MyButton2 from "../../components/Buttons/MyButton2";
+import MyButton1 from "../../components/Buttons/MyButton1";
 
 const SingleBlogVlog = () => {
+  const navigate = useNavigate();
+
   return (
     <section className="bg-background1 dark:bg-dark_background1 text-primary_text dark:text-dark_primary_text py-4 px-16 duration-300 min-h-screen flex flex-col items-center gap-5">
       <div className="flex flex-col justify-center items-center gap-4 ">
@@ -35,8 +41,23 @@ const SingleBlogVlog = () => {
         />
       </div>
 
+      {/* state details */}
+      <div className="flex items-center w-full justify-evenly">
+        <h1 className=" font-pangaia text-xl font-bold">State: West Bengal</h1>
+        <h1 className=" font-pangaia text-xl font-bold">City: Kolkata</h1>
+        <a
+          href="https://maps.app.goo.gl/Vu6YMT5cNhP39wkT9"
+          target="_blank"
+          className={`hollowBorder blogCards font-searchBars  text-lg p-2 rounded-full bg-transparent text-primary_text dark:text-dark_primary_text `}
+        >
+          <CiLocationOn className=" w-6 h-6" />
+        </a>
+      </div>
+
       {/* blog content / vlog caption */}
-      <div className="  text-xl">
+      <div className="bg-background1 dark:bg-dark_background1 z-10">
+        Ekhane React Quill Editor er output ta display korabi <br />
+        <br />
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
         minima, nemo error sequi accusantium consequuntur cum sint ullam
         incidunt nisi minus vitae quis debitis, repudiandae exercitationem,
