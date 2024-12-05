a0c148ce21e8d53cb749fc6546289ec96d2c40af
Pratik Biswas <04biswaspratik@gmail.com>
2024-12-05 16:21:53 +0530

    admin and instructor page UI is updated

--- client/src/pages/Career/CareerTeacherForm.jsx
+++ client/src/pages/Career/CareerTeacherForm.jsx
@@ -4,8 +4,12 @@ import TextareaComponent from "../../components/Textarea/TextareaComponent";
 import InputImageVideo from "../../components/Input/InputImageVideo";
 import MyButton4 from "../../components/Buttons/MyButton4";
 import img1 from "../../assets/career/admins.png";
+import { useTranslation } from "react-i18next";
 
 const CareerTeacherForm = () => {
+  const { t } = useTranslation();
+  const careerInstructorContent = t("CareerData", { returnObjects: true });
+
   const [formData, setFormData] = useState({
     fullName: "",
     email: "",
@@ -60,73 +64,72 @@ const CareerTeacherForm = () => {
     >
       <div className="px-20 py-1 flex flex-col items-start justify-start gap-5 ">
         <h1 className="text-4xl font-extrabold border-2 rounded-3xl border-black mt-14 px-3 py-2 ">
-          Join Us as an Instructor
+          {careerInstructorContent.careerInstructor.heading}
         </h1>
         <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          molestiae debitis. Nobis suscipit porro officia quae, eaque nesciunt.
-          Eveniet eaque reprehenderit ut deserunt non ipsa cumque autem expedita
-          inventore distinctio.
+          {careerInstructorContent.careerInstructor.description}
         </p>
 
         <div className="space-y-4 w-full">
           <div className="flex justify-between w-full gap-5">
             <div>
-              <h2 className="text-2xl font-bold mb-3">Eligibility Criteria</h2>
+              <h2 className="text-2xl font-bold mb-3">
+                {careerInstructorContent.careerInstructor.sH1}
+              </h2>
               <ul className="list-disc list-inside text-lg">
-                <li>
-                  Bachelor's degree in Computer Science, IT, or related field
-                </li>
-                <li>
-                  Proficiency in website management tools (React, Tailwind, CMS)
-                </li>
-                <li>Strong problem-solving skills and attention to detail</li>
-                <li>Prior experience as a website administrator (preferred)</li>
-                <li>Excellent communication and organizational skills</li>
+                {careerInstructorContent.careerInstructor.sH1List.map(
+                  (content, ind) => (
+                    <li key={ind}>{content.des}</li>
+                  )
+                )}
               </ul>
             </div>
 
             <div>
-              <h2 className="text-2xl font-bold mb-3">Responsibilities</h2>
+              <h2 className="text-2xl font-bold mb-3">
+                {careerInstructorContent.careerInstructor.sH2}
+              </h2>
               <ul className="list-disc list-inside text-lg">
-                <li>Maintain and update website content</li>
-                <li>Troubleshoot technical issues and ensure website uptime</li>
-                <li>Monitor website performance and implement improvements</li>
-                <li>
-                  Collaborate with other departments to ensure smooth operations
-                </li>
+                {careerInstructorContent.careerInstructor.sH2List.map(
+                  (content, ind) => (
+                    <li key={ind}>{content.des}</li>
+                  )
+                )}
               </ul>
             </div>
           </div>
           <div className="flex w-full justify-between">
             <div className="flex flex-col gap-8">
               <div>
-                <h2 className="text-2xl font-bold mb-3">Job Details</h2>
+                <h2 className="text-2xl font-bold mb-3">
+                  {careerInstructorContent.careerInstructor.sH3}
+                </h2>
                 <ul className="list-disc list-inside text-lg">
-                  <li>
-                    <strong>Salary:</strong> ₹25,000 - ₹35,000/month
-                    (Negotiable)
-                  </li>
-                  <li>
-                    <strong>Job Type:</strong> Full-Time (Remote/On-Site)
-                  </li>
-                  <li>
-                    <strong>Location:</strong> Remote or Office-Based
-                  </li>
+                  {careerInstructorContent.careerInstructor.sH3List.map(
+                    (content, ind) => (
+                      <li key={ind}>{content.des}</li>
+                    )
+                  )}
                 </ul>
               </div>
 
               <div>
-                <h2 className="text-2xl font-bold mb-3">Benefits</h2>
+                <h2 className="text-2xl font-bold mb-3">
+                  {careerInstructorContent.careerInstructor.sH4}
+                </h2>
                 <ul className="list-disc list-inside text-lg">
-                  <li>Flexible working hours</li>
-                  <li>Paid time off (PTO)</li>
-                  <li>Performance bonuses</li>
-                  <li>Professional development opportunities</li>
+                  {careerInstructorContent.careerInstructor.sH4List.map(
+                    (content, ind) => (
+                      <li key={ind}>{content.des}</li>
+                    )
+                  )}
                 </ul>
               </div>
             </div>
-            <img src={img1} className=" w-[25rem] h-[25rem]" />
+            <img
+              src={careerInstructorContent.careerInstructor.careerImg}
+              className=" w-[25rem] h-[25rem]"
+            />
           </div>
         </div>
       </div>
@@ -209,12 +212,15 @@ const CareerTeacherForm = () => {
 
         <InputComponent
           iType={"text"}
-          iName={"Languages Known"}
+          iName={"Domain of Teaching"}
           required={true}
           // value={formData.education}
         />
 
-        <InputImageVideo imageName={`Upload Resume/CV:`} fileType="file" />
+        <InputImageVideo
+          imageName={`Upload Resume/CV with photo:`}
+          fileType="file"
+        />
         {/* Submit */}
 
         <div className=" w-1/3">
