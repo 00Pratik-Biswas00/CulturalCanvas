b2ad727a96170991954c41df404aecf463748081
sattwikeeg100 <sattwikeeghosh95@gmail.com>
2024-12-08 14:19:06 +0530

    [Add] Explicit content verification for blog

--- ml-server/app.py
+++ ml-server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from routes.budget_routes import budget_bp
 from routes.nearest_attractions_routes import attractions_bp
+from routes.nude_detection_routes import nude_detection_routes
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,7 @@ CORS(app)
 # Register Blueprints
 app.register_blueprint(budget_bp)
 app.register_blueprint(attractions_bp)
+app.register_blueprint(nude_detection_routes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
