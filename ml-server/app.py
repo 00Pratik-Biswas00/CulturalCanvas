9a7d0b9de4d516fa90dd4d7139de15d194ef8c87
sattwikeeg100 <sattwikeeghosh95@gmail.com>
2024-12-09 07:37:33 +0530

    [Update] Language Translation done

--- ml-server/app.py
+++ ml-server/app.py
@@ -1,16 +1,25 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from routes.budget_routes import budget_bp
-from routes.nearest_attractions_routes import attractions_bp
-from routes.nude_detection_routes import nude_detection_routes
+from routes.nearest_attractions_routes import nearest_attractions_bp
+from routes.nude_detection_routes import nude_detection_bp
+from routes.lang_translation_routes import translation_bp
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+cors_origin = os.getenv("FLASK_CORS_ORIGIN", "*")  # Fallback to '*' if not set
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=[cors_origin])
 
 # Register Blueprints
 app.register_blueprint(budget_bp)
-app.register_blueprint(attractions_bp)
-app.register_blueprint(nude_detection_routes)
+app.register_blueprint(nearest_attractions_bp)
+app.register_blueprint(nude_detection_bp)
+app.register_blueprint(translation_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
