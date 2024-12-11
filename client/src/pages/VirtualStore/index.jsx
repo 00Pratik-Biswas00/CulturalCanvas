81763a7b576055d25e8f8e11553a4ee563eda8ca
Pratik Biswas <04biswaspratik@gmail.com>
2024-12-11 22:30:21 +0530

    store ui is updated

--- client/src/pages/VirtualStore/index.jsx
+++ client/src/pages/VirtualStore/index.jsx
@@ -4,11 +4,6 @@ import car1 from "../../assets/store/a.mp4";
 import img1 from "../../assets/store/aaa.png";
 import { CiSearch, CiFilter } from "react-icons/ci";
 
-import car2 from "../../assets/store/ed.mp4";
-import { FaAngleDoubleDown } from "react-icons/fa";
-import { FaAngleDoubleUp } from "react-icons/fa";
-import { TfiMoreAlt } from "react-icons/tfi";
-import MyButton1 from "../../components/Buttons/MyButton1";
 import MyButton2 from "../../components/Buttons/MyButton2";
 
 const cards = [
@@ -118,32 +113,54 @@ const VirtualStore = () => {
             onMouseLeave={handleMouseLeave}
           >
             {flippedCard !== index ? (
-              <>
+              <div className=" p-3 flex items-center justify-center flex-col">
                 {hoveredCard === index ? (
                   <video
                     src={car.video}
                     autoPlay
                     loop
-                    className="w-full h-full"
+                    className="w-full h-full mb-5"
                   />
                 ) : (
                   <img
                     src={car.image}
                     alt={car.heading}
-                    className="w-[20rem] h-[20rem]"
+                    className="w-[20rem] h-[20rem] mb-5"
                   />
                 )}
 
-                <div className="p-3">
-                  <button onClick={() => handleFlip(index)}>NEXT</button>
-                </div>
-              </>
+                <button
+                  onClick={() => handleFlip(index)}
+                  className={`relative group overflow-hidden px-6 h-12 rounded-full flex space-x-2 items-center bg-gradient-to-r from-[#193c70e9] to-[#1489386c] hover:to-[#174926] `}
+                >
+                  <span className="relative text-base text-dark_primary_text">
+                    NEXT
+                  </span>
+                  <div className="flex items-center -space-x-3 translate-x-3">
+                    <div className="w-2.5 h-[1.6px] rounded bg-white origin-left scale-x-0 transition duration-300 group-hover:scale-x-100"></div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 stroke-white -translate-x-2 transition duration-300 group-hover:translate-x-0"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 5l7 7-7 7"
+                      />
+                    </svg>
+                  </div>
+                </button>
+              </div>
             ) : (
-              <div className="back absolute inset-0 flex flex-col justify-center items-center bg-background2 rounded-lg">
+              <div className="back absolute inset-0 flex flex-col justify-center items-center rounded-lg">
                 <div className="containerx">
                   <div className="boxx">
                     <h2 className="namex">Khadi Kurti</h2>
-                    <a href="/" className="buyx">
+                    <a href="/virtual-store/id" className="buyx">
                       Buy
                     </a>
                     <div className="circlex"></div>
