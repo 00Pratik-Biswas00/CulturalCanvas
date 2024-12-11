e11b3ec561ce2a054fc1291f6febb40b3f20a85f
Pratik Biswas <04biswaspratik@gmail.com>
2024-12-12 04:48:32 +0530

    explore updates

--- client/src/pages/ExploreDiversity/index.jsx
+++ client/src/pages/ExploreDiversity/index.jsx
@@ -6,6 +6,9 @@ import { FaPlus } from "react-icons/fa6";
 import travelling from "../../assets/explorePlaces/exploreDiversity.png";
 import trip from "../../assets/explorePlaces/createTripPoster.png";
 import money from "../../assets/explorePlaces/predictPoster.png";
+import story from "../../assets/explorePlaces/story.png";
+import rag from "../../assets/explorePlaces/summary.png";
+import nearAttraction from "../../assets/explorePlaces/nearAt.png";
 import { switchLoginModalOpen } from "../../redux/slices/loginModalOpenSlice";
 import MyButton3 from "../../components/Buttons/MyButton3";
 
@@ -31,42 +34,6 @@ const ExploreDiversity = () => {
           Explore Rich Cultural Heritages and Traditions of India
         </h1>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div
-            className={` p-5 rounded-lg flex flex-col items-center shadow-lg shadow-gray-400 gap-5 `}
-          >
-            <div className="flex flex-col">
-              <img src={money} alt=".." className="w-full h-full rounded-md" />
-            </div>
-            <h2 className="text-3xl font-extrabold text-center uppercase">
-              No idea how much it will cost?{" "}
-            </h2>
-            <p className="">
-              Planning a heritage tour in India can be exciting, but the cost
-              can sometimes be a mystery. With our easy-to-use tool, you can
-              quickly estimate the potential expenses for your trip. <br />{" "}
-              <br />{" "}
-              <em>
-                <b>
-                  Get started now and plan your dream Indian heritage tour with
-                  confidence!
-                </b>
-              </em>
-            </p>
-
-            <MyButton3
-              classDesign={
-                "before:bg-highlight_hover_dark bg-highlight_hover text-dark_primary_text "
-              }
-              onClick={() => {
-                navigate(`predict-budget`);
-              }}
-              buttonName={"Predict Budget"}
-              buttonIcon={<RiMoneyRupeeCircleFill />}
-            />
-          </div>
-
-          {/* 2 */}
-
           <div
             className={` p-5 rounded-lg flex flex-col items-center shadow-lg shadow-gray-400 gap-5`}
           >
@@ -106,49 +73,11 @@ const ExploreDiversity = () => {
             />
           </div>
 
-          {/* 3 */}
-
           <div
             className={` p-5 rounded-lg flex flex-col items-center shadow-lg shadow-gray-400 gap-5 `}
           >
             <div className="flex flex-col">
-              <img src={money} alt=".." className="w-full h-full rounded-md" />
-            </div>
-            <h2 className="text-3xl font-extrabold text-center uppercase">
-              Want to know what is near to you?{" "}
-            </h2>
-            <p className="">
-              Planning a heritage tour in India can be exciting, but the cost
-              can sometimes be a mystery. With our easy-to-use tool, you can
-              quickly estimate the potential expenses for your trip. <br />{" "}
-              <br />{" "}
-              <em>
-                <b>
-                  Get started now and plan your dream Indian heritage tour with
-                  confidence!
-                </b>
-              </em>
-            </p>
-
-            <MyButton3
-              classDesign={
-                "before:bg-highlight_hover_dark bg-highlight_hover text-dark_primary_text "
-              }
-              onClick={() => {
-                navigate(`nearest-attractions`);
-              }}
-              buttonName={"Nearest Attractions"}
-              buttonIcon={<RiMoneyRupeeCircleFill />}
-            />
-          </div>
-
-          {/* 4 */}
-
-          <div
-            className={` p-5 rounded-lg flex flex-col items-center shadow-lg shadow-gray-400 gap-5 `}
-          >
-            <div className="flex flex-col">
-              <img src={money} alt=".." className="w-full h-full rounded-md" />
+              <img src={story} alt=".." className="w-full h-full rounded-md" />
             </div>
             <h2 className="text-3xl font-extrabold text-center uppercase">
               Story Telling{" "}
@@ -178,13 +107,83 @@ const ExploreDiversity = () => {
             />
           </div>
 
-          {/* 5 */}
-
           <div
             className={` p-5 rounded-lg flex flex-col items-center shadow-lg shadow-gray-400 gap-5 `}
           >
             <div className="flex flex-col">
               <img src={money} alt=".." className="w-full h-full rounded-md" />
+            </div>
+            <h2 className="text-3xl font-extrabold text-center uppercase">
+              No idea how much it will cost?{" "}
+            </h2>
+            <p className="">
+              Planning a heritage tour in India can be exciting, but the cost
+              can sometimes be a mystery. With our easy-to-use tool, you can
+              quickly estimate the potential expenses for your trip. <br />{" "}
+              <br />{" "}
+              <em>
+                <b>
+                  Get started now and plan your dream Indian heritage tour with
+                  confidence!
+                </b>
+              </em>
+            </p>
+
+            <MyButton3
+              classDesign={
+                "before:bg-highlight_hover_dark bg-highlight_hover text-dark_primary_text "
+              }
+              onClick={() => {
+                navigate(`predict-budget`);
+              }}
+              buttonName={"Predict Budget"}
+              buttonIcon={<RiMoneyRupeeCircleFill />}
+            />
+          </div>
+
+          <div
+            className={` p-5 rounded-lg flex flex-col items-center shadow-lg shadow-gray-400 gap-5 `}
+          >
+            <div className="flex flex-col">
+              <img
+                src={nearAttraction}
+                alt=".."
+                className="w-full h-full rounded-md"
+              />
+            </div>
+            <h2 className="text-3xl font-extrabold text-center uppercase">
+              Want to know what is near to you?{" "}
+            </h2>
+            <p className="">
+              Planning a heritage tour in India can be exciting, but the cost
+              can sometimes be a mystery. With our easy-to-use tool, you can
+              quickly estimate the potential expenses for your trip. <br />{" "}
+              <br />{" "}
+              <em>
+                <b>
+                  Get started now and plan your dream Indian heritage tour with
+                  confidence!
+                </b>
+              </em>
+            </p>
+
+            <MyButton3
+              classDesign={
+                "before:bg-highlight_hover_dark bg-highlight_hover text-dark_primary_text "
+              }
+              onClick={() => {
+                navigate(`nearest-attractions`);
+              }}
+              buttonName={"Nearest Attractions"}
+              buttonIcon={<RiMoneyRupeeCircleFill />}
+            />
+          </div>
+
+          <div
+            className={` p-5 rounded-lg flex flex-col items-center shadow-lg shadow-gray-400 gap-5 `}
+          >
+            <div className="flex flex-col">
+              <img src={rag} alt=".." className="w-full h-full rounded-md" />
             </div>
             <h2 className="text-3xl font-extrabold text-center uppercase">
               RAG Manual Summary{" "}
@@ -210,42 +209,6 @@ const ExploreDiversity = () => {
                 navigate(`rag-manual-summary`);
               }}
               buttonName={"RAG Manual Summary"}
-              buttonIcon={<RiMoneyRupeeCircleFill />}
-            />
-          </div>
-
-          {/* 6 */}
-
-          <div
-            className={` p-5 rounded-lg flex flex-col items-center shadow-lg shadow-gray-400 gap-5 `}
-          >
-            <div className="flex flex-col">
-              <img src={money} alt=".." className="w-full h-full rounded-md" />
-            </div>
-            <h2 className="text-3xl font-extrabold text-center uppercase">
-              Ancient Script Transcription{" "}
-            </h2>
-            <p className="">
-              Planning a heritage tour in India can be exciting, but the cost
-              can sometimes be a mystery. With our easy-to-use tool, you can
-              quickly estimate the potential expenses for your trip. <br />{" "}
-              <br />{" "}
-              <em>
-                <b>
-                  Get started now and plan your dream Indian heritage tour with
-                  confidence!
-                </b>
-              </em>
-            </p>
-
-            <MyButton3
-              classDesign={
-                "before:bg-highlight_hover_dark bg-highlight_hover text-dark_primary_text "
-              }
-              onClick={() => {
-                navigate(`ancient-script-transcription`);
-              }}
-              buttonName={"Ancient Script Transcription"}
               buttonIcon={<RiMoneyRupeeCircleFill />}
             />
           </div>
