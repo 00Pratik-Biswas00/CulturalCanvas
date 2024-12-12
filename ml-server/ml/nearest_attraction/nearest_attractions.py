cb2296a97e235eeebc53a3daa2b326cebc8c0285
sattwikeeg100 <sattwikeeghosh95@gmail.com>
2024-12-12 15:16:18 +0530

    [Update] Nearest Attractions model

--- ml-server/ml/nearest_attraction/nearest_attractions.py
+++ ml-server/ml/nearest_attraction/nearest_attractions.py
@@ -1,19 +1,25 @@
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 import overpy
+import requests
 
 geolocator = Nominatim(user_agent="nearest_attraction_finder")
 api = overpy.Overpass()
 
-# Function to get coordinates of a place
-def get_coordinates(place_name):
-    location = geolocator.geocode(place_name)
-    if location:
-        return (location.latitude, location.longitude)
-    else:
-        return None
+GOOGLE_MAPS_API_KEY = "AIzaSyAtsC9mZQ6RbN6A9TvGU40yXtxJhVGX9uE"
 
-# Function to find nearest places based on user input
+def get_user_coordinates():
+    url = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + GOOGLE_MAPS_API_KEY
+    payload = {"considerIp": True}
+
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        location_data = response.json().get("location")
+        return (location_data["lat"], location_data["lng"])
+    else:
+        raise Exception("Failed to fetch user location. Ensure Google Maps API is correctly configured.")
+
 def find_nearest_places(user_location, attraction_type, radius=30000):
     attraction_types = {
         "tourist_spots": '["tourism"~"attraction|museum|monument|zoo|theme_park|viewpoint"]',
@@ -37,13 +43,39 @@ def find_nearest_places(user_location, attraction_type, radius=30000):
             attraction_location = (node.lat, node.lon)
             distance = geodesic(user_location, attraction_location).km
             google_maps_link = f"https://www.google.com/maps?q={node.lat},{node.lon}"
-            
-            attractions.append({
-                'name': node.tags.get("name"),
-                'location': attraction_location,
-                'distance': distance,
-                'link': google_maps_link
-            })
+
+            details = get_place_details(node.tags.get("name"))
+
+            if details:
+                attractions.append({
+                    'name': details['name'],
+                    'rating': details['rating'],
+                    'address': details['address'],
+                    'photo_reference': details['photo_reference'],
+                    'distance': round(distance, 2),
+                    'link': google_maps_link
+                })
 
     attractions_sorted = sorted(attractions, key=lambda x: x['distance'])
     return attractions_sorted[:20]
+
+
+def get_place_details(place_name):
+    url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key={GOOGLE_MAPS_API_KEY}&input={place_name}&inputtype=textquery&fields=name,rating,formatted_address,photos"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data.get("candidates"):
+            place = data["candidates"][0]
+            details = {
+                "name": place.get("name"),
+                "rating": place.get("rating", "N/A"),
+                "address": place.get("formatted_address", "N/A"),
+                "photo_reference": None
+            }
+
+            if "photos" in place:
+                photo_reference = place["photos"][0].get("photo_reference")
+                details["photo_reference"] = photo_reference
+            return details
+    return None
